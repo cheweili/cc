8f34004fc9576aa20abd3eaaaded6edb0a0f1fbe
TEST <https://github.com/cheweili/MyTest1.git>
2016-11-01 13:54:15 +0800

    fast time

---
+++ app/src/main/java/com/example/cofco/mytest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.cofco.mytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        imageView = new ImageView(this);
+
+    }
+}
